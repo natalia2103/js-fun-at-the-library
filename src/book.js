ff80c7c486d0af34eea159bb111a56fe628736b2
Natalia Buchmiller <nataliambuchmiller@gmail.com>
2021-03-25 20:15:46 -0700

    Finish book-test.js

--- src/book.js
+++ src/book.js
@@ -32,28 +32,46 @@ function saveReview(newReview, reviews) {
 }
 
 function saveReview(newReview, reviews) {
+  if (!reviews.length) {
+    reviews.push(newReview);
+  }
   for (var i = 0; i < reviews.length; i++) {
-    for (var j = i+1; j < reviews.length; j++) {
-      if(reviews[i] === reviews[j]) {
-        for (var k = j; k < reviews.length; k++) {
-          reviews[k] = reviews[k + 1];
-      }
-      reviews.length--;
-      j--;
-    }
-  return reviews.push(newReview);
+    if (newReview === reviews[i]) {
+      return reviews;
+  } else {
+      reviews.push(newReview);
+      return reviews
+  }
 }
-}
+return reviews;
 }
 
 
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+
+function writeBook(title, character, genre) {
+  return newBook = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  };
+}
+
+function editBook(newBook) {
+  newBook.pageCount = newBook.pageCount * .75
+  return newBook.pageCount;
+}
 
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
