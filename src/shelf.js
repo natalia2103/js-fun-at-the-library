a4a8a82318d75671ac1750bb9f842f8010eecd69
Natalia Buchmiller <nataliambuchmiller@gmail.com>
2021-03-26 17:32:26 -0700

    Finish shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,47 @@
 
+function shelfBook(title, sciFiShelf) {
+  // console.log('title', title);
+  // console.log('shelf', sciFiShelf);
+  if (sciFiShelf.length < 3) {
+      return sciFiShelf.unshift(title);
+    }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      shelf.splice(i, 1);
+    }
+  }
+  return shelf;
+}
+
+function listTitles(shelf) {
+  var list = [];
+  for (var i = 0; i < shelf.length; i++){
+    list.push(shelf[i].title);
+  }
+  return list.join(", ");
+}
+
+
+function searchShelf(shelf, title) {
+  var result = false;
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      result = true;
+      break;
+    } else {
+      result = false;
+    }
+  }
+  return result;
+}
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
