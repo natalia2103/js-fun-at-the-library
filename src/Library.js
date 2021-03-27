4e6b64abffbcde312ce0326133f2dcd3017f19ff
Natalia Buchmiller <nataliambuchmiller@gmail.com>
2021-03-27 11:30:26 -0700

    Finish library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,42 @@
 
+function createLibrary(libraryName) {
+  return library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
+
+function addBook(library, title) {
+  if (title.genre === "fantasy") {
+      return library.shelves.fantasy.push(title);
+  } else if (title.genre === "fiction") {
+      return library.shelves.fiction.push(title);
+  } else {
+      return library.shelves.nonFiction.push(title);
+  }
+}
+
+
+function checkoutBook(library, bookTitle, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (bookTitle === library.shelves[genre][i].title) {
+      library.shelves[genre].splice(i, 1);
+      return `You have now checked out ${bookTitle} from the ${library.name}`
+      }
+    }
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+  }
+
+
+
+// string interpolation
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
